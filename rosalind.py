3c3eebf9c44da2f8eec9c858020c9a2358722ded
spookyboogy <matt.tolstoy@gmail.com>
2015-11-11 18:45:28 -0600

    Still not quite robust.

--- rosalind.py
+++ rosalind.py
@@ -1246,7 +1246,7 @@ def edges_to_form_tree(graph_file):
 	"""
 	graph_file -> A file formatted as follows:
 			          n
-					  <adjacency list>
+			          <adjacency list>
 				  Where n is the number of nodes in a graph containing
 				  no cycles. The adjacencies should be listed as follows:
 					  1 2
